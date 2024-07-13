d39b4b42307db533b92029ebd641babbb0a497fd
MyLifeGithub <diyor.aslanov2012@gmail.com>
2024-07-13 14:49:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+let obj = {
+    age: 20,
+    name: 'Alex'
+}
+console.log(obj);
+
+let a = 'name';
+
+console.log(obj.name);
+console.log(obj[a]);
+
+obj.isMarried = false; 
+
+console.log(obj);
+// let key = prompt()
+
+// if (obj[key]) {
+//     alert('key already exist')
+// } else {
+//     let value = prompt()
+//     obj[key] = value
+// }
+let arr = [1,2,3]
+arr.forEach(item => {
+    console.log(item.lenght);
+});
+let name = prompt()
+arr.push(name) // add to end
+arr.shift() // remove from start
+arr.unshift(name) // add to start
+arr.pop() // remove from end
+
+let b = {
+    names: [],
+    user: {
+        name: 'Alex'
+    }
+}
+b.names.push('HELLO WORLD')
+console.log(b.user[name]);
+
+let newArr = [4,3,12,17]
+newArr.forEach(element => {
+    if(element % 2 == 0) {
+        console.log(element);
+    }
+});
